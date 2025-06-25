d1120fc7591c2af12f59d58b51d965faa1cc5783
Artika-A <artikaafaisal@gmail.com>
2025-06-25 22:07:26 +0800

    Revision

--- session7/index.js
+++ session7/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-app.use(express.json())  //tell express that it can accept j.son
+app.use(express.json())  //middleware //tell app that now it can accept j.son
 
 function logger(req, res, next) {
     console.log(`${req.method} ${req.url}`);
@@ -40,8 +40,22 @@ app.get("/products", (req, res) => {  //read
         res.json(products)
 })
 
+
+// app.post("/products", (req, res) => {
+//     const pid = req.body.id;
+//     const productName = req.body.name
+//     const newProduct = {
+//         id: pid,
+//         name: productName
+//     }
+//     products.push(newProduct)
+//     res.json(newProduct)
+
+// })
+
+
 app.post("/products", (req, res) => {
-    const lastProduct = newProduct[products.length -1]
+    const lastProduct=products[products.length - 1]
     const lastId = lastProduct.id
     const pid = lastId + 1
     const productName = req.body.name
@@ -54,25 +68,28 @@ app.post("/products", (req, res) => {
 })
 
 
-
-
 app.post("/message", (req, res) => {
     const message = req.body.text;
     res.send(`You said: ${message}`);
 });
 
+// app.put("/products/:pid", (req, res) => {
+//     const pid = req.params.pid
+//     const indexOfProduct = products.findIndex(product => product.id == pid)
+//     res.send(indexOfProduct)
+// })
 
 app.put("/products/:pid", (req, res) => {
     const pid = req.params.pid
     const newProductName = req.body.name
-    const indexOfProduct = products.findindex(product => product.id == pid)
-    product[indexOfProduct].name = newProductName
+    const indexOfProduct = products.findIndex(product => product.id == pid)
+    products[indexOfProduct].name = newProductName
     res.json(products[indexOfProduct])
-})
+}) //update
 
 app.delete("/products/:pid", (req, res) => {
     const pid = req.params.pid
-    const indexOfProduct = products.findindex(product => product.id == pid)
+    const indexOfProduct = products.findIndex(product => product.id == pid)
     products.splice(indexOfProduct, 1)
     res.json(products)
 })
